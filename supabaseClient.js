ccef70a373a3ee9890a23a339fb1135770b513ae
smurfeth <rosefethcatadman@gmail.com>
2026-09-23 22:31:39 +0800

    Initial commit

---
+++ supabaseClient.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://bnytmuwgutotekaaylig.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_c5fAj53uud1ud8WFRY_fxg_WOvW2kI1';
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
